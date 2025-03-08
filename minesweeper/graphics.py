9ab697f77b5479787fed7560364df6557e90f0b9
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2025-03-09 00:01:01 +0100

    Simplify assets dir

--- minesweeper/graphics.py
+++ minesweeper/graphics.py
@@ -1,14 +1,14 @@
-from pathlib import Path
-from importlib.metadata import version
 import random
+from importlib.metadata import version
+from pathlib import Path
 
-from pyglet import window, image
+from pyglet import image, window
 from pyglet.image import AbstractImage
 
 from minesweeper.taxonomy import Piece
 
-
 ASSET_DIR = Path(__name__).parent.parent / "assets"
+
 
 class MinesweeperWindow(window.Window):
     CAPTION: str = f"Minesweeper - v{version('minesweeper')}"
@@ -27,27 +27,26 @@ class MinesweeperWindow(window.Window):
         self.pause = False
 
     def load_images(self) -> dict[Piece, AbstractImage]:
-        board_dir = ASSET_DIR / "board"
         return {
-            Piece.BASE: image.load(str(board_dir / "base.png")),
-            Piece.BOMB: image.load(str(board_dir / "bomb.png")),
-            Piece.ONE: image.load(str(board_dir / "one.png")),
-            Piece.TWO: image.load(str(board_dir / "two.png")),
-            Piece.THREE: image.load(str(board_dir / "three.png")),
-            Piece.FOUR: image.load(str(board_dir / "four.png")),
-            Piece.FIVE: image.load(str(board_dir / "five.png")),
-            Piece.SIX: image.load(str(board_dir / "six.png")),
-            Piece.SEVEN: image.load(str(board_dir / "seven.png")),
-            Piece.EIGHT: image.load(str(board_dir / "eight.png")),
-            Piece.EMPTY: image.load(str(board_dir / "empty.png")),
-            Piece.FLAG: image.load(str(board_dir / "flag.png")),
+            Piece.BASE: image.load(str(ASSET_DIR / "base.png")),
+            Piece.BOMB: image.load(str(ASSET_DIR / "bomb.png")),
+            Piece.ONE: image.load(str(ASSET_DIR / "one.png")),
+            Piece.TWO: image.load(str(ASSET_DIR / "two.png")),
+            Piece.THREE: image.load(str(ASSET_DIR / "three.png")),
+            Piece.FOUR: image.load(str(ASSET_DIR / "four.png")),
+            Piece.FIVE: image.load(str(ASSET_DIR / "five.png")),
+            Piece.SIX: image.load(str(ASSET_DIR / "six.png")),
+            Piece.SEVEN: image.load(str(ASSET_DIR / "seven.png")),
+            Piece.EIGHT: image.load(str(ASSET_DIR / "eight.png")),
+            Piece.EMPTY: image.load(str(ASSET_DIR / "empty.png")),
+            Piece.FLAG: image.load(str(ASSET_DIR / "flag.png")),
         }
 
-    def on_draw(self):
+    def on_draw(self) -> None:
         if not self.pause:
             for x in range(0, self.WIDTH - 64, 64):
                 for y in range(0, self.HEIGHT - 64, 64):
-                    piece = random.choice(range(1, 13))
+                    piece = Piece(random.choice(range(1, 13)))
                     self.images[piece].blit(x, y, 0)
 
             self.pause = True
